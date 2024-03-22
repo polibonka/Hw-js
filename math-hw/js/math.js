4dc377cabc870f1ab5ac05c944f422d98de61d57
polibonka <polibonka@gmail.com>
2024-03-22 17:12:45 +0000

    new files

---
+++ math-hw/js/math.js
@@ -0,0 +1,46 @@
+"use strict";
+
+//1
+let celcius = 20;
+let ferengeit = (9 / 5) * celcius + 32;
+console.log(ferengeit);
+
+//2
+let days = 30;
+let hours = 24 * days;
+let min = 60 * hours;
+console.log(hours);
+console.log(min);
+
+//3
+let hp = 100;
+let energy = 50;
+
+hp -= 20;
+energy -= 10;
+
+console.log(hp);
+console.log(energy);
+
+//4
+let sum = 100;
+let discount = sum * 0.1;
+console.log(discount);
+
+//5
+let num = 2.7;
+console.log(Math.floor(num));
+
+//6
+let st = "12.7df76";
+console.log(parseFloat(st));
+
+//7
+let str = "5hehe";
+console.log(parseInt(str));
+
+// 8 - не знаю
+
+//9
+let stri = "10px";
+console.log(parseInt(stri));
---
+++ start_js-hw/js/hw.js
@@ -0,0 +1,39 @@
+"use strict";
+
+//1
+let age = 15;
+console.log(age);
+
+//2
+let name = "Polina";
+console.log(name);
+
+//3
+let isStudent = true;
+let isNotStudent = false;
+console.log(isStudent);
+console.log(isNotStudent);
+
+//4
+let quote = "If you want it, for work for it";
+console.log(quote);
+
+//5
+let myNumber = 10;
+console.log(myNumber + 10);
+
+//6
+let myNull = null;
+console.log(myNull);
+
+//7
+const yourName = prompt("What is your name?");
+console.log(yourName);
+
+//7
+const message = confirm("Confirm your choice");
+console.log(message);
+
+//8
+alert("confirm");
+console.log(message);
