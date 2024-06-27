c7c90c3fed4ca96b42ae7566f533debb6e723e26
Polina <polibonka@gmail.com>
2024-06-27 19:19:55 +0300

    hw-12/13

--- objects-methods/js/objects-methods-hw.js
+++ objects-methods/js/objects-methods-hw.js
@@ -7,28 +7,28 @@ const user = {
   premium: true,
 };
 
-user.mood = 'happy'
-user.hobby = 'skydiving';
-user.premium = false
+const { hobby: userHobby, premium: userPremium } = user;
+
+user.mood = "happy";
+user.hobby = "skydiving";
+user.premium = false;
 console.log(user);
-
-
 
 const users = Object.keys(user);
 
 for (let keys of users) {
-console.log(`keys: ${keys}, value: ${user[keys]}`);
+  console.log(`keys: ${keys}, value: ${user[keys]}`);
 }
 
 //2
 
 const countProps = (obj) => {
-  
   const properties = {
     property1: "house",
     property2: "car",
   };
-const numProperties = Object.keys(properties).length
+  const{ property, property2 } = properties;
+  const numProperties = Object.keys(properties).length;
 
   return numProperties;
 };
@@ -36,85 +36,67 @@ console.log(countProps());
 
 // 3
 
-
 const findBestEmployee = (employees) => {
-let bestEmployee = ''
-let maxTask = 0;
-for (let employee in  employees) {
-  if (employees[employee] > 0) {
-maxTask = employees[employee]
-bestEmployee = employee
-}  
-}
-return bestEmployee
+  let bestEmployee = "";
+  let maxTask = 0;
+  for (let employee in employees) {
+    if (employees[employee] > 0) {
+      maxTask = employees[employee];
+      bestEmployee = employee;
+    }
+  }
+  return bestEmployee;
 };
 
+console.log(findBestEmployee({ Ann: 5, David: 8, Helen: 11 }));
 
-console.log(findBestEmployee({  Ann: 5,
-  David: 8,
-    Helen: 11,}));
-    
 // 4
 const countTotalSalary = function (employees) {
-let total = 0;
- for(let employee in employees){
-total += employees[employee]
-}
-return total
+  let total = 0;
+  for (let employee in employees) {
+    total += employees[employee];
+  }
+  return total;
 };
-console.log(countTotalSalary({ mango: 100,
-  poly: 150,
-  alfred: 80,}));
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
 
+// 5
 
-  // 5
+const products1 = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроїд", price: 400, quantity: 7 },
+  { name: "Захоплення", price: 1200, quantity: 2 },
+];
+const { name: name1, price: price1, quantity: quantity1 } = products1[0];
+const { name: name2, price: price2, quantity: quantity2 } = products1[1];
+const { name: name3, price: price3, quantity: quantity3 } = products1[2];
+const { name: name4, price: price4, quantity: quantity4 } = products1[3];
 
-  const products1 = [
-    { name: 'Радар', price: 1300, quantity: 4 },
-    { name: 'Сканер', price: 2700, quantity: 3 },
-    { name: 'Дроїд', price: 400, quantity: 7 },
-    { name: 'Захоплення', price: 1200, quantity: 2 },
-  ];
 
 const getAllPropValues = function (arr, prop) {
-  return arr.map(item => item [prop])
+  return arr.map((item) => item[prop]);
 };
-console.log(getAllPropValues(products, 'name'));
-  
-
-
+console.log(getAllPropValues(products1, "name"));
 
 //6
 
 const products = [
-  { name: 'Радар', price: 1300, quantity: 4 },
-  { name: 'Сканер', price: 2700, quantity: 3 },
-  { name: 'Дроїд', price: 400, quantity: 7 },
-  { name: 'Захоплення', price: 1200, quantity: 2 },
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроїд", price: 400, quantity: 7 },
+  { name: "Захоплення", price: 1200, quantity: 2 },
 ];
-
-const[
-  item1,item2,item3,item4 
-] = products
-const{name: name1, price: price1,  quantity: quantity1} = item1
-const{name: name2, price: price2,  quantity: quantity2} = item2
-const{name: name3, price: price3,  quantity: quantity3} = item3
-const{name: name4, price: price4,  quantity: quantity4} = item4
-
-console.log(item1);
-console.log(item2);
-console.log(name1);
+const { name: nameProducts1, price: priceProducts1, quantity: quantityProducts1 } = products[0];
+const { name: nameProducts2, price: priceProducts2, quantity: quantityProducts2 } = products[1];
+const { name: nameProducts3, price: priceProducts3, quantity: quantityProducts3 } = products[2];
+const { name: nameProducts4, price: priceProducts4, quantity: quantityProducts4 } = products[3];
 
 const calculateTotalPrice = function (allProdcuts, productName) {
-  // твій код
+  const product = allProdcuts.find((product) => product.name === productName);
+  return product ? product.price * product.quantity : 0;
 };
-
-/*
- * Викличи функції для перевірки працездатності твоєї реалізації.
- */
-console.log(calculateTotalPrice(products, 'Радар')); // 5200
-
-console.log(calculateTotalPrice(products, 'Дроїд')); // 2800
+console.log(calculateTotalPrice(products, "Радар"));
 
 //7
 
@@ -123,10 +105,10 @@ console.log(calculateTotalPrice(products, 'Дроїд')); // 2800
  * Можна покласти або зняти гроші з рахунку.
  */
 const Transaction = {
-  DEPOSIT: 'deposit',
-  WITHDRAW: 'withdraw',
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
 };
-
+const { DEPOSIT, WITHDRAW } = Transaction;
 /*
  * Кожна транзакція - це об'єкт з властивостями: id, type і amount
  */
@@ -143,8 +125,8 @@ const account = {
    * Приймає суму і тип транзакції.
    */
   createTransaction(amount, type) {
-    const id = this.transactions.length
-    return {id, type, amount}
+    const id = this.transactions.length;
+    return { id, type, amount };
   },
 
   /*
@@ -154,9 +136,9 @@ const account = {
    * після чого додає його в історію транзакцій
    */
   deposit(amount) {
-    const transaction = this.createTransaction(amount, Transaction.DEPOSIT)
-   this.transactions.push(transaction)
-   this.balance += amount
+    const transaction = this.createTransaction(amount, Transaction.DEPOSIT);
+    this.transactions.push(transaction);
+    this.balance += amount;
   },
 
   /*
@@ -169,28 +151,26 @@ const account = {
    * про те, що зняття такої суми не можливо, недостатньо коштів.
    */
   withdraw(amount) {
-    if(this.balance < amount){
-      console.log('Недостатньо коштів');
+    if (this.balance < amount) {
+      console.log("Недостатньо коштів");
     }
-    const transaction = this.createTransaction(amount,Transaction.WITHDRAW)
-    this.transactions.push(transaction)
-    this.balance -= amount
+    const transaction = this.createTransaction(amount, Transaction.WITHDRAW);
+    this.transactions.push(transaction);
+    this.balance -= amount;
   },
 
   /*
    * Метод повертає поточний баланс
    */
   getBalance() {
-   return  this.balance
+    return this.balance;
   },
 
   /*
    * Метод шукає і повертає об'єкт транзакції по id
    */
   getTransactionDetails(id) {
-   return  this.transactions.find(item => item.id === id) 
-
-     
+    return this.transactions.find((item) => item.id === id);
   },
 
   /*
@@ -198,20 +178,20 @@ const account = {
    * певного типу транзакції з усієї історії транзакцій
    */
   getTransactionTotal(type) {
-    return this.transactions.filter((item) => item.type === type)
+    return this.transactions.filter((item) => item.type === type);
   },
 };
 
-account.deposit(500)
+account.deposit(500);
 console.log(account.getBalance());
 
-account.deposit(1000)
+account.deposit(1000);
 console.log(account.getBalance());
 
-account.withdraw(500)
+account.withdraw(500);
 console.log(account.getBalance());
 
-account.withdraw(200)
+account.withdraw(200);
 console.log(account.getBalance());
 
 console.log(account.transactions);
