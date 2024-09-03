987b538745d3e222e7075c60c38b82157e96b312
Polina <polibonka@gmail.com>
2024-09-03 15:42:05 +0300

    hw-19

--- events/js/events-hw.js
+++ events/js/events-hw.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//1, 2
 const btnModalOpen = document.querySelector('.button[data-action="open-modal"]')
 const btnModalClose = document.querySelector('.button[data-action="close-modal"]')
 const backdrop = document.querySelector('.js-backdrop')
@@ -20,3 +20,49 @@ backdrop.addEventListener('click', (e)=> {
         toggleModal()
     }
 })
+
+//3
+
+const radios = document.querySelectorAll('input[name="color"]');
+
+radios.forEach(radio => {
+    radio.addEventListener('change', (event) => {
+        const selectedColor = event.target.value;
+        document.body.style.backgroundColor = selectedColor;
+    });
+});
+
+//4
+
+const inputField = document.getElementById('name-input');
+const outputField = document.getElementById('name-output');
+
+inputField.addEventListener('input', function() {
+    outputField.textContent = inputField.value.trim() === '' ? 'незнайомець' : inputField.value;
+});
+
+
+inputField.addEventListener('blur', function() {
+    const expectedLength = parseInt(inputField.getAttribute('data-length'));
+    const actualLength = inputField.value.length;
+
+    if (actualLength === expectedLength) {
+        inputField.classList.add('valid');
+        inputField.classList.remove('invalid');
+    } else {
+        inputField.classList.add('invalid');
+        inputField.classList.remove('valid');
+    }
+});
+
+//5
+
+const fontSizeControl = document.getElementById('font-size-control');
+const text = document.getElementById('text');
+
+fontSizeControl.addEventListener('input', function() {
+    const fontSize = fontSizeControl.value + 'px';
+    text.style.fontSize = fontSize;
+});
+
+
