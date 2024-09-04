86c08db677641f42155ad7ccef1fd24b98196e10
Polina <polibonka@gmail.com>
2024-09-04 14:11:33 +0300

    hw-18

--- DOM/js/Dom-2.js
+++ DOM/js/Dom-2.js
@@ -45,27 +45,47 @@ const ingredients = [
 
 const images = [
     {
-     url:
-      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
      alt: 'White and Black Long Fur Cat',
     },
     {
-     url:
-      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
     },
     {
-     url:
-      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
      alt: 'Group of Horses Running',
     },
-   ];
+];
 
-const galleryContainer = document.querySelector('#gallery')
+const galleryContainer = document.querySelector('#gallery');
+
 const galleryItems = images.map(image => {
-    return `<li class='galletyItem'>
-    <img src=''${image.url}' alt = '${image.alt}'/><li>`
-    
-})
+    return `<li class='galleryItem'>
+                <img src='${image.url}' alt='${image.alt}' />
+            </li>`;
+}).join(''); 
 
-galleryContainer.insertAdjacentElement('beforeend', galleryItems)
+galleryContainer.insertAdjacentHTML('beforeend', galleryItems); 
+
+//4
+
+let counterValue = 0;
+
+const valueEl = document.getElementById('value');
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+const increment = () => {
+    counterValue += 1;
+    valueEl.textContent = counterValue; 
+};
+
+const decrement = () => {
+    counterValue -= 1;
+    valueEl.textContent = counterValue; 
+};
+
+
+decrementBtn.addEventListener('click', decrement);
+incrementBtn.addEventListener('click', increment);
