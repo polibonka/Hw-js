ef9b81f1e838b59a9a8679839f384e5388078237
Polina <polibonka@gmail.com>
2025-01-15 19:22:17 +0200

    new-hw

--- observer-hw/js/index.js
+++ observer-hw/js/index.js
@@ -1,35 +1,3 @@
-// const images = document.querySelectorAll('img')
-
-// const preloadImage = (img) => {
-//     const src = img.getAttribute('data-src');
-//     if (!src) return;
-
-//     img.src = src;
-//     img.onload = () => {
-//       img.classList.add('loaded'); // Додайте клас для переходів CSS
-//     };
-//   };
-
-//   const observerOptions = {
-//     threshold: 0.5, 
-//   };
-
-//   const observer = new IntersectionObserver((entries, observer) => {
-//     entries.forEach((entry) => {
-//       if (entry.isIntersecting) {
-//         const image = entry.target
-//         image.src = image.dataset.src
-//         image.classList.add("img-loaded")
-//         observer.unobserve(image)
-//       }
-//     });
-//   }, observerOptions);
-
-//   images.forEach((image) => {
-//     observer.observe(image);
-//   });
-
-
 document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll('img');
 
@@ -44,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     const observerOptions = {
-        threshold: 0.5, // 50% зображення повинно бути видимим
+        threshold: 0.5,
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
